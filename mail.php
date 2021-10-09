0545aef5f639fe342a8a2ae0d3041d4a9bea25df
Gladys <wkanyora10@gmail.com>
2021-10-09 10:29:28 +0300

    new changes done

--- mail.php
+++ mail.php
@@ -1,27 +1,27 @@
-<?php
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+<!-- 
+// $name = $_POST['name'];
+// $visitor_email = $_POST['email'];
+// $subject = $_POST['subject'];
+// $message = $_POST['message'];
 
-$email_from = 'wkanyora10@gmail.coma';
+// $email_from = 'wkanyora10@gmail.coma';
 
-$email_subject = 'New Form Submission';
+// $email_subject = 'New Form Submission';
 
-$email_body = "User Name: $name.\n".
-              "User Email: $visitor_email.\n".
-              "Subject: $subject.\n".
-              "User Message: $message.\n";
+// $email_body = "User Name: $name.\n".
+//               "User Email: $visitor_email.\n".
+//               "Subject: $subject.\n".
+//               "User Message: $message.\n";
               
-  $to = 'kangladys18@gmail.com';
+//   $to = 'kangladys18@gmail.com';
 
-  $headers = "From: $email_from \r\n";
+//   $headers = "From: $email_from \r\n";
 
-  $headers .= "Reply-To: $visitor_email \r\n";
+//   $headers .= "Reply-To: $visitor_email \r\n";
 
 
-  mail($to,$email_subject,$email_subject,$headers);
+//   mail($to,$email_subject,$email_subject,$headers);
 
-  header("location: contact.html");
+//   header("location: contact.html");
 
-?>
+?> -->
