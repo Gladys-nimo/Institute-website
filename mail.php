cddc80f5bc136c6dc94beb7e828b4e767829df5f
programBreaker247 <cliffordmorara45@gmail.com>
2021-10-10 05:06:17 +0300

    Updated javascript link

--- mail.php
+++ mail.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
@@ -24,4 +24,4 @@ $email_body = "User Name: $name.\n".
 
   header("location: contact.html");
 
-?>
+?> -->
