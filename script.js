f2138bb50216afe7c5efbb05ef8f779daaf61a2e
programBreaker247 <cliffordmorara45@gmail.com>
2021-10-11 09:58:43 +0300

    implementations

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ $(document).ready(function() {
     })
 })
 
-
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
@@ -16,6 +15,38 @@ function showMenu() {
 }
 function hideMenu() {
     navLinks.style.right = "-200px";
+}
+
+function open_panel() {
+    slideIt();
+    var a = document.getElementById("sidebar");
+    a.setAttribute("id", "sidebar1");
+    a.setAttribute("onclick", "close_panel()");
+}
+
+function slideIt() {
+    var slidingDiv = document.getElementById("slider");
+    var stopPosition = 0;
+    if (parseInt(slidingDiv.style.right) < stopPosition) {
+        slidingDiv.style.right = parseInt(slidingDiv.style.right) + 2 + "px";
+        setTimeout(slideIt, 1)
+    }
+}
+
+function close_panel() {
+    slideIn();
+    a = document.getElementById("sidebar1");
+    a.setAttribute("id", "sidebar");
+    a.setAttribute("onclick", "open_panel()");
+}
+
+function slideIn() {
+    var slidingDiv = document.getElementById("slider");
+    var stopPosition = -342;
+    if (parseInt(slidingDiv.style.right) > stopPosition) {
+        slidingDiv.style.right = parseInt(slidingDiv.style.right) -2 + "px";
+        setTimeout(slideIn, 1);
+    }
 }
 
 
