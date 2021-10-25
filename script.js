72c3adaf8ba58b931b8db866913df0d6056a8f51
programBreaker247 <cliffordmorara45@gmail.com>
2021-10-25 06:41:12 +0300

    Added a form

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-$(document).ready(function() {
-    $(window).scroll(function(){
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        } else {
-            $('.navbar').removeClass("sticky");
-        }
-    });
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) {
+      $(".navbar").addClass("sticky");
+    } else {
+      $(".navbar").removeClass("sticky");
+    }
+  });
 });
+
 
 var navLinks = document.getElementById("navLinks");
 
 function showMenu() {
-    navLinks.style.right = "0";
+  navLinks.style.right = "0";
 }
 function hideMenu() {
-    navLinks.style.right = "-200px";
+  navLinks.style.right = "-200px";
 }
 
-const spans = document.querySelectorAll('.word span');
+const spans = document.querySelectorAll(".word span");
 spans.forEach((span, idx) => {
-    span.addEventListener('scroll', (e) => {
-        e.target.classList.add('active');
-    });
-    span.addEventListener('animationend', (e) => {
-        e.target.classList.remove('active');
-    });
-    setTimeout(() => {
-        span.classList.add('active');
-    }, 750 * (idx + 1))
+  span.addEventListener("scroll", (e) => {
+    e.target.classList.add("active");
+  });
+  span.addEventListener("animationend", (e) => {
+    e.target.classList.remove("active");
+  });
+  setTimeout(() => {
+    span.classList.add("active");
+  }, 750 * (idx + 1));
 });
-
-
 
 
